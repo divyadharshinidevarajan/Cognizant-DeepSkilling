97238ba0b870495cdc6660f1887453661ec11340
DIVYADHARSHINI D <147182832+divyadharshinidevarajan@users.noreply.github.com>
2025-07-13 18:45:07 +0530

    Adding Week 4  hands-on
    Spring REST using Spring Boot 3

---
+++ Week4_Spring REST using Spring Boot 3/2. spring-rest-handson/Hello World RESTful Web Service/code/src/main/java/com/cognizant/springlearn2Hw/Springlearn2HwApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn2Hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlearn2HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlearn2HwApplication.class, args);
+	}
+
+}
---
+++ Week4_Spring REST using Spring Boot 3/2. spring-rest-handson/REST - Country Web Service/code/src/main/java/com/cognizant/springlearn2RESTALLCty/Springlearn2RestallCtyApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn2RESTALLCty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlearn2RestallCtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlearn2RestallCtyApplication.class, args);
+	}
+
+}
